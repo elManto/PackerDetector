d3e8a6b47ba3fe93e1e9c789b8c698b0b93322d3
elmanto <alk13@hotmail.it>
2018-10-25 15:33:17 +0200

    packer_analyser.sh ended

--- packer_analyser.sh
+++ packer_analyser.sh
@@ -4,7 +4,7 @@ sample=$1
 record=$sample
 mkdir $sample
 echo "Analysing sample "$sample
-$PANDA/i386-softmmu/qemu-system-i386 -m 2048 -hda win7x86.img -replay $record -panda osi -os windows-32-7 -panda syscalls2:profile=windows_7_x86 -panda exec_mem_track:proc_name=$sample
+$PANDA/i386-softmmu/qemu-system-i386 -m 2048 -hda win7x86.img -replay $record -panda osi -os windows-32-7 -panda syscalls2:profile=windows_7_x86 -panda exec_mem_track:proc_name=$sample -panda net:proc_name=$sample
 echo "First plugin ended its execution!"
 if [ -s memory_detection.txt ]
 then
@@ -14,6 +14,7 @@ else
 	echo "File 'memory_detection' empty"
 	python win7_syscall_name.py /home/elmanto/behaviors.txt
 	mv behavioral_analysis.txt $sample/
+	mv flagged_behaviors.txt $sample
 	echo "not packed/virtual env detection" > result.txt
 fi
 mv module_detection.txt $sample/
@@ -21,7 +22,7 @@ mv memory_detection.txt $sample/
 mv behaviors.txt $sample/
 mv asid.txt $sample/
 mv result.txt $sample/
-rm $record-rr-*
-mv $sample/ Analysed/
-mv LowEntropySamples1/$sample Analysed_sample/
+mv network_detection.txt $sample/
+#rm $record-rr-*
+
 
